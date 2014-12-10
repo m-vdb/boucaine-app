d82460643fbad444c0e4e2078ac67b2758b0c59f
mvergerdelbove <mvergerdelbove@work4labs.com>
2014-12-09 23:08:39 -0800

    scan controller calls verifyCode

--- www/js/controllers/scan.js
+++ www/js/controllers/scan.js
@@ -1,14 +1,19 @@
-module.exports = function ($scope, $ionicLoading) {
+module.exports = function ($scope, $ionicLoading, verifyCode) {
   $scope.startScan = function () {
     $ionicLoading.show({
       template: 'Scan en cours...'
     });
     cordova.plugins.barcodeScanner.scan(function (result) {
-      $ionicLoading.hide();
-      $scope.result = result.text;
+      verifyCode(result.text).done(function () {
+        $ionicLoading.hide();
+        $scope.result = "Successfully scanned " + result.text;
+      }).fail(function (xhr) {
+        $ionicLoading.hide();
+        $scope.result = "XHR Error";
+      });
     }, function (error) {
       $ionicLoading.hide();
-      $scope.loader = "Error.";
+      $scope.result = "Scan Error.";
     });
   };
 };
