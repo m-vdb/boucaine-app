63ea3245788086d6015612eb73b438dc5ed818ae
mvergerdelbove <mvergerdelbove@work4labs.com>
2014-12-13 11:22:42 +0100

    read the error from the XHR

--- www/js/controllers/scan.js
+++ www/js/controllers/scan.js
@@ -3,17 +3,19 @@ module.exports = function ($scope, $ionicLoading, verifyCode) {
     $ionicLoading.show({
       template: 'Scan en cours...'
     });
+
     cordova.plugins.barcodeScanner.scan(function (result) {
       verifyCode(result.text).done(function () {
         $ionicLoading.hide();
         $scope.result = "Successfully scanned " + result.text;
       }).fail(function (xhr) {
         $ionicLoading.hide();
-        $scope.result = "XHR Error";
+        var msg = (xhr.data && xhr.data.message) ? xhr.data.message : "Une erreur est survenue, veuillez réessayer.";
+        $scope.result = msg;
       });
     }, function (error) {
       $ionicLoading.hide();
-      $scope.result = "Scan Error.";
+      $scope.result = "Scan impossible, veuillez réessayer.";
     });
   };
 };
