de280350476de38eafe69ff3f4ccf86337ae4cc7
Source <loicvergerdelbove@Loics-MacBook-Pro.local>
2014-12-29 01:17:29 +0100

    Google Maps, logo, mise en place des menus, responsive design

--- www/js/app.js
+++ www/js/app.js
@@ -8,7 +8,7 @@
 
 var api = "http://boucaine-api.herokuapp.com";
 
-angular.module('starter', ['ionic', 'restangular'])
+angular.module('starter', ['ionic', 'restangular','ngMap'])
 
 /***** Constants *****/
 .constant('PROMO_GOAL', 10)
@@ -26,6 +26,51 @@ angular.module('starter', ['ionic', 'restangular'])
   'PROMO_GOAL', require('./controllers/scan.js')])
 .controller('ContactCtrl', ['$scope', require('./controllers/contact.js')])
 .controller('MenuCtrl', ['$scope', require('./controllers/menu.js')])
+.controller('MapController', function($scope, $ionicLoading) {
+
+  $scope.positions = [{
+    lat: 43.43771774793634,
+    lng: 5.424571931362152
+  }];
+
+  $scope.$on('mapInitialized', function(event, map) {
+    $scope.map = map;
+  });
+
+  $scope.centerOnMe= function(){
+  $scope.positions = [];
+    
+    
+    $ionicLoading.show({
+      template: 'Loading...'
+    });
+
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      $scope.positions.push({lat: pos.k,lng: pos.B});
+      console.log(pos);
+      $scope.map.setCenter(pos);
+      $ionicLoading.hide();
+    });
+
+  };
+
+  $scope.gameLocationClicked = function() {
+    //pass control to mobile nav system
+    AnalyticsTracker.event('gameDetail.gameLocationClicked');
+
+    //default - works on android - http://habaneroconsulting.com/insights/opening-native-map-apps-from-the-mobile-browser#.VDEf6PmSx8E 
+    var geoUrl = "geo: " + $scope.positions.Latitude + ", " + $scope.positions.Longitude;
+    if (ionic.Platform.isIOS()) {
+    //https://developer.apple.com/library/ios/featuredarticles/iPhoneURLScheme_Reference/MapLinks/MapLinks.html
+      geoUrl = 'maps:ll=' + $scope.positions.Latitude + ", " + $scope.positions.Longitude;
+    }                    
+    window.location = geoUrl;   
+  }
+
+})
+ 
 /***** Run *****/
 .run(['$ionicPlatform', function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -99,3 +144,4 @@ angular.module('starter', ['ionic', 'restangular'])
     });
   }
 ]);
+
