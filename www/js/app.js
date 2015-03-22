b1a2e735ff927d9016e8f70b49b8c9ebdf3c3fd7
Vdeub <loicvdbts1@gmail.com>
2015-03-22 13:05:52 +0000

    Action bar bottom + Finition App android

--- www/js/app.js
+++ www/js/app.js
@@ -42,13 +42,15 @@ angular.module('starter', ['ionic', 'restangular','ngMap'])
   });
 }])
 /***** Config *****/
-.config(['$stateProvider', '$urlRouterProvider',
-  function($stateProvider, $urlRouterProvider) {
+.config(['$stateProvider', '$urlRouterProvider','$ionicConfigProvider',
+  function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
 
     // Ionic uses AngularUI Router which uses the concept of states
     // Learn more here: https://github.com/angular-ui/ui-router
     // Set up the various states which the app can be in.
     // Each state's controller can be found in controllers.js
+    $ionicConfigProvider.tabs.position('bottom');
+    $ionicConfigProvider.tabs.style('standard');
     $stateProvider
 
       // setup an abstract state for the tabs directive
